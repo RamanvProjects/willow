f864b7a081ffea6f7d1aa901b3a83193b05f2f9f
Pato Ricaud <pato.ricaud@gmail.com>
2017-11-20 15:32:58 -0600

    Fixed shape dim error

--- python/main.py
+++ python/main.py
@@ -1,7 +1,7 @@
 from models.gan3d import Discriminator3d
 import dummy
 
-data = dummy.generate_data(5, (64, 64, 64))
+data = dummy.generate_data(5, (64, 64, 64, 1))
 k = Discriminator3d(True, 5, 0.001, 64, 15)
 
 k.predict(data)
