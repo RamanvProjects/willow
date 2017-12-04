3c9e331c76f436716d5ba8b491b546e8e1be01d3
Patricio Ricaud <pato.ricaud@gmail.com>
2017-12-04 18:16:24 -0500

    Sorting by height dim, and parsing all dir

--- python/preprocess.py
+++ python/preprocess.py
@@ -1,19 +1,37 @@
 """ To parse weird data is to become weird data
 """
 from collections import defaultdict
+from numpy import vstack, argsort
 import json
+from os import listdir 
 
-test_json = open('data/json/graph.12001.obj.json').read() # '{"1,2,3":["1,2,3"], "4,6,5":["1,2,3","8,9,10"]}'
+# test_json = open('data/json/graph.00001.obj.json').read() # '{"1,2,3":["1,2,3"], "4,6,5":["1,2,3","8,9,10"]}'
 
-k = json.loads(test_json)
-j = defaultdict(list)
+def parse_dir(dir_name):
+	parsed_trees = []
+	for file_name in listdir(dir_name):
+		parsed_trees.append(process_file(file_name))
+	return parsed_trees
 
-for l, v in k.iteritems():
-    l = tuple(map(float, l.split(',')))
-    
-    for q in v:
-        a, b, c = map(float, q.split(','))
-        j[l].append((a, b, c))
+def parse_file(file_name):
+	json_file = open(file_name).read()
+	k = json.loads(json_file)
+	j = defaultdict(list)
 
-print j.keys()
+	for l, v in k.iteritems():
+	    l = tuple(map(float, l.split(',')))
+	    
+	    for q in v:
+	        a, b, c = map(float, q.split(','))
+	        j[l].append((a, b, c))
+
+	verts = vstack(j.keys())
+	sorted_verts = verts[argsort(verts[:, 1])]
+
+	return sorted_verts
+
+
+
+# Testing one file
+print parse_file('data/json/graph.00001.obj.json')
 
